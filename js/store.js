7889dd2b72286ce71542630d8ce2629496b1b863
KNMods-Dayz <reliablevaleting@gmail.com>
2026-01-18 21:34:27 +0000

    Render product cards in the product grid

--- js/store.js
+++ js/store.js
@@ -1,1 +1,17 @@
 
+const grid = document.getElementById("product-grid");
+
+Object.keys(PRODUCTS).forEach(id => {
+    const p = PRODUCTS[id];
+
+    const card = `
+        <div class="product-card">
+            <img src="${p.image}">
+            <h3>${p.name}</h3>
+            <p>${p.price} Credits</p>
+            <a href="product.html?id=${id}" class="btn">View</a>
+        </div>
+    `;
+
+    grid.innerHTML += card;
+});
